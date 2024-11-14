56a6eb9032824262c0f97c18497f90546d28da9c
Prithvijai <prithvijai0510@gmail.com>
2024-11-13 22:39:06 -0700

    Simple trajectory planning done

--- mycobot_600_planner_cpp/src/hello_world.cpp
+++ mycobot_600_planner_cpp/src/hello_world.cpp
@@ -21,9 +21,9 @@ int main(int argc, char* argv[])
   auto const target_pose = [] {
     geometry_msgs::msg::Pose msg;
     msg.orientation.w = 1.0;
-    msg.position.x = 0.0;
-    msg.position.y = 0.1817;
-    msg.position.z = 0.817;
+    msg.position.x = 0.4;
+    msg.position.y = 0.2;
+    msg.position.z = 0.3;
     return msg;
   }();
   move_group_interface.setPoseTarget(target_pose);
